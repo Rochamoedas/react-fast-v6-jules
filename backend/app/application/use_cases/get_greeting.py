686ddec0363cedc1e9c6f3773431180c08818e33
Rochamoedas <charankorocha@gmail.com>
2025-05-24 04:22:27 -0300

    Delete backend directory

--- backend/app/application/use_cases/get_greeting.py
+++
@@ -1,9 +0,0 @@
-from app.domain.entities.message import Message
-
-
-class GetGreetingUseCase:
-    """Use case for retrieving a greeting message."""
-    
-    def execute(self) -> Message:
-        """Get a greeting message."""
-        return Message("Hello, World!")
--- backend/app/domain/interfaces/o_data_repository.py
+++
@@ -1,5 +0,0 @@
-from typing import List, Dict, Any
-
-class ODataRepository:
-    def save(self, data: List[Dict[str, Any]]) -> None:
-        raise NotImplementedError("This method should be overridden by subclasses")
